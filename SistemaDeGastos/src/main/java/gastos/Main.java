3f8fdef8a694ef226896fb0668df0291aa658d05
SrMassive <xmassiveg@gmail.com>
2023-06-14 18:59:54 -0300

    Codigo de Criação de Banco de Dados

--- SistemaDeGastos/src/main/java/gastos/Main.java
+++ SistemaDeGastos/src/main/java/gastos/Main.java
@@ -2,12 +2,16 @@ package gastos;
 
 import java.sql.SQLException;
 
+import BD.createDB;
 import BD.userCadDAO;
 
 public class Main {
     
     public static void main(String[] args) throws SQLException {
         
+        createDB DB = new createDB();
+        DB.createBD();
+
         userCadDAO contaE = new userCadDAO();
         //Começa o aplicativo, chamando a tela de login
         if(contaE.contaExist().next()){
